429e2034ddf8cc5512fabe31790cac72c31c6f00
Alexandre Gobjila <alexandre.gobjila@lp-digital.fr>
2016-06-13 16:33:08 +0200

    fix collection duplicates for config persistor
    fix collection duplicates for config persistor

--- Collection/Collection.php
+++ Collection/Collection.php
@@ -109,14 +109,17 @@ class Collection
     public static function array_merge_assoc_recursive($array1, $array2)
     {
         foreach ($array2 as $key => $value) {
-            if (is_int($key)) {
+
+            if (is_int($key) && !in_array($array2[$key], $array1, true)) {
                 $array1[] = $array2[$key];
             } elseif (!array_key_exists($key, $array1)) {
                 $array1[$key] = $value;
             } elseif (is_array($value) && is_array($array1[$key]) ) {
                 $array1[$key] = self::array_merge_assoc_recursive($array1[$key], $value);
             } else {
-                $array1[$key] = $value;
+                if (!in_array($value, $array1, true)) {
+                    $array1[$key] = $value;
+                }
             }
         }
 
